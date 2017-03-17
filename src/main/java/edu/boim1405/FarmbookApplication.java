94d0f9854145395ab331a4c4bb140f8f67e73f93
Bors Odon <borsodon@gmail.com>
2017-03-17 22:47:22 +0200

    Initial commit

---
+++ src/main/java/edu/boim1405/FarmbookApplication.java
@@ -0,0 +1,12 @@
+package edu.boim1405;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmbookApplication.class, args);
+	}
+}
